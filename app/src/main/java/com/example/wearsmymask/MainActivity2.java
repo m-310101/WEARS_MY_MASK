08e9e51c181cf2683834526fa0725226051cb299
chloeliu1 <39767170+chloeliu1@users.noreply.github.com>
2021-03-16 18:46:55 +0000

    Update MainActivity2.java

--- app/src/main/java/com/example/wearsmymask/MainActivity2.java
+++ app/src/main/java/com/example/wearsmymask/MainActivity2.java
@@ -10,5 +10,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
 }
